00046eb1029eb17d6be3444ec20648f1e142090d
LeeGwonSeon <42333578+LeeGwonSeon@users.noreply.github.com>
2022-04-20 21:59:56 +0900

    PorfolioUpdate_js and css page

---
+++ src/js/portfolio.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('a-page-scroll').bind('click', function(event){
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop:
+            $($anchor.attr('herf')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-toggleLvisible').click(function(){
+    $('.navbar-toggle:visible').click();
+}); 
